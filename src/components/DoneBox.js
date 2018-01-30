99f1b0e5edf841eb45e4fc485e405645f5099053
Olof Landahl <olof.landahl@hiq.se>
2018-01-30 17:56:48 +0100

    Updated done box styling

--- src/components/DoneBox.js
+++ src/components/DoneBox.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import FaClose from 'react-icons/lib/fa/close'
+import FaClose from 'react-icons/lib/fa/close';
+import FaClone from 'react-icons/lib/fa/clone';
+import FaTh from 'react-icons/lib/fa/th';
+import FaCheck from 'react-icons/lib/fa/check';
+import FaClockO from 'react-icons/lib/fa/clock-o';
 import './DoneBox.css';
 
 const DoneBox = ({ doneStats, onCloseDoneBoxClick }) => {
@@ -11,23 +15,36 @@ const DoneBox = ({ doneStats, onCloseDoneBoxClick }) => {
     doneStats = {};
   }
 
+  let sec = doneStats.seconds;
+  let min = toWhole(sec);
+  sec = toRest(sec);
+  let h = toWhole(min);
+  min = toRest(min);
+  const time = [h, min, sec]
+    .map(x => withPad(x.toString()))
+    .join(':');
+
   return ( 
     <div className={className}>
       <div className="done-box">
         <div className="done-box-inner">
-          <div>Matches: {doneStats.noOfMatches}</div>
-          <div>Cards: {doneStats.noOfCards}</div>
+          <div className="done-item"><FaClone className="test" /> {doneStats.noOfMatches}</div>
+          <div className="done-item"><FaTh /> {doneStats.noOfCards}</div>
           <br />
-          <div>Fails: {doneStats.noOfFails}</div>
-          <div>Successes: {doneStats.noOfSuccesses}</div>
+          <div className="done-item"><FaCheck /> {doneStats.noOfSuccesses}</div>
+          <div className="done-item"><FaClose /> {doneStats.noOfFails}</div>
           <br />
-          <div>Seconds: {doneStats.seconds}</div>
+          <div className="done-item"><FaClockO /> {time}</div>
           <span className="close-icon" onClick={onCloseDoneBoxClick}><FaClose /></span>
         </div>
       </div>
     </div> 
   );
 };
+
+const toWhole = x => (x ? Math.floor(x / 60) : 0);
+const toRest = x => (x % 60);
+const withPad = x => ('00'.substring(0, 2 - x.length) + x);
 
 DoneBox.propTypes = {
   doneStats: PropTypes.object,
